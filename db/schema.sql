57e0d8b75f1419f64189971e3c8b539eb16d706c
Phillip <Phillip@MacBook-Pro.local>
2021-05-13 14:33:19 -0700

    added some functions

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE team_member(
+  first_name VARCHAR(35),
+  last_name VARCHAR(35),
+  role_id INT,
+  manager_id INT
+  
+);
+
+CREATE TABLE e_role(
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+
+);
+
+CREATE TABLE department(
+    d_name VARCHAR(30)
+);
---
+++ serve.js
@@ -0,0 +1,105 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: '361010theone',
+  database: 'employee_db',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('hi good to goo')
+  runSearch()
+});
+
+const runSearch = () => {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees',
+        'View By Department',
+        'View By Manager',
+        'Add Employee',
+        'Remove Employee',
+        'Update Role',
+        'Update Manager',
+        'View Roles',
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'View All Employees':
+          employeeSearch();
+          break;
+
+        case 'View By Department':
+          departmentSearch();
+          break;
+
+        case 'View By Manager':
+          managerSearch();
+          break;
+
+        case 'Add Employee':
+          addEmployee();
+          break;
+
+        case 'Remove Employee':
+          removeEmployee();
+          break;
+
+          case 'Update Role':
+          updateRole();
+          break;
+
+        case 'Update Manager':
+          updateManager();
+          break;
+
+        case 'View Roles':
+          roleSearch();
+          break;
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+};
+
+
+const employeeSearch = () => {
+
+    connection.query('SELECT * FROM team_member', (err,res)=>{
+      if (err) throw err;
+      res.forEeach(({first_name, last_name, role_id, manager_id})=>{
+        console.log(`${first_name} | ${last_name} | ${role_id} | ${manager_id}`)
+      });
+      console.log('-------------------------')
+    });
+
+};
+
+const departmentSearch = () => {
+
+  connection.query('SELECT * FROM e_role', (err,res)=>{
+    if (err) throw err;
+    res.forEeach(({first_name, last_name, role_id, manager_id})=>{
+      console.log(`${first_name} | ${last_name} | ${role_id} | ${manager_id}`)
+    });
+    console.log('-------------------------')
+  });
+
+};
