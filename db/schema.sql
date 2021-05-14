aafa4ab128a37b4d99b03d87b8b98b4cb3a3c434
Philly <Phil.stew@live.ca>
2021-05-13 20:11:34 -0700

    hi

--- db/schema.sql
+++ db/schema.sql
@@ -3,28 +3,65 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE team_member(
-  first_name VARCHAR(35),
-  last_name VARCHAR(35),
-  role_id INT,
-  manager_id INT
-  
+
+CREATE TABLE department(
+    id INT,
+    d_name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE e_role(
+    id INT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
+    PRIMARY KEY (id)
 
 );
+CREATE TABLE team_member(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(35),
+  last_name VARCHAR(35),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
 
-    d_name VARCHAR(30)
+  
 );
-<<<<<<< HEAD
-=======
-f
+
+
+INSERT INTO department (id, d_name)
+VALUES(1, 'Sales Rep');
+
+INSERT INTO department (id, d_name)
+VALUES(2, 'Customer Service');
+
+INSERT INTO department (id, d_name)
+VALUES(3, 'Cashier');
+
+INSERT INTO department (id, d_name)
+VALUES(4, 'Merchandiser');
+
+INSERT INTO department (id, d_name)
+VALUES(5, 'Inventory Control');
+
+INSERT INTO department (id, d_name)
+VALUES(6, 'Administration');
+
+
+INSERT INTO e_role (id, title, salary, department_id)
+VALUES(1, 'Manager', 90000.00, 1);
+
+INSERT INTO e_role (id, title, salary, department_id)
+VALUES(2, 'Supervisor', 60000.00, 1);
+
+INSERT INTO e_role (id, title, salary, department_id)
+VALUES(2, 'Team Member', 60000.00, 1);
+
+
+
 INSERT INTO team_member (first_name, last_name, role_id, manager_id)
-VALUES("Phil", "Stew", 3, 9);
+VALUES("Phil", "Stew", 1, 1);
 
 INSERT INTO team_member (first_name, last_name, role_id, manager_id)
 VALUES("Philn", "Stewn", 3, 9);
@@ -34,4 +71,3 @@ VALUES("Philnp", "Stewnl", 3, 9);
 
 INSERT INTO team_member (first_name, last_name, role_id, manager_id)
 VALUES("Philnp", "Stewnl", 3, 9);
->>>>>>> c7cde567e8428ee13f854a0ff77cea8f21e65c33
