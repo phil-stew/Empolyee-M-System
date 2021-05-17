a84b130d7ce467148cdc8c9de35155ea480d2e91
Philly <Phil.stew@live.ca>
2021-05-16 21:44:05 -0400

    updates functions

--- db/schema.sql
+++ db/schema.sql
@@ -6,11 +6,11 @@ USE employee_db;
 
 CREATE TABLE department(
     id INT NOT NULL,
-    d_name VARCHAR(30),
+    department VARCHAR(30),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE e_role(
+CREATE TABLE role(
     id INT,
     title VARCHAR(30),
     salary DECIMAL,
@@ -18,7 +18,7 @@ CREATE TABLE e_role(
     PRIMARY KEY (id)
 
 );
-CREATE TABLE team_member(
+CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(35),
   last_name VARCHAR(35),
@@ -30,51 +30,55 @@ CREATE TABLE team_member(
 );
 
 
-INSERT INTO department (id, d_name)
+INSERT INTO department (id, department)
 VALUES(1, 'Sales Rep');
 
-INSERT INTO department (id, d_name)
+INSERT INTO department (id, department)
 VALUES(2, 'Customer Service');
 
-INSERT INTO department (id, d_name)
+INSERT INTO department (id, department)
 VALUES(3, 'Cashier');
 
-INSERT INTO department (id, d_name)
+INSERT INTO department (id, department)
 VALUES(4, 'Merchandiser');
 
-INSERT INTO department (id, d_name)
+INSERT INTO department (id, department
 VALUES(5, 'Inventory Control');
 
-INSERT INTO department (id, d_name)
+INSERT INTO department (id, department
 VALUES(6, 'Administration');
 
 
-INSERT INTO e_role (id, title, salary, department_id)
+INSERT INTO role (id, title, salary, department_id)
 VALUES(1, 'Manager', 90000.00, 1);
 
-INSERT INTO e_role (id, title, salary, department_id)
+INSERT INTO role (id, title, salary, department_id)
 VALUES(2, 'Supervisor', 60000.00, 1);
 
-INSERT INTO e_role (id, title, salary, department_id)
+INSERT INTO role (id, title, salary, department_id)
 VALUES(3, 'Team Member', 60000.00, 1);
 
 
 
-INSERT INTO team_member (first_name, last_name, role_id, manager_id)
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES("Phil", "Stew", 1, 1);
 
-INSERT INTO team_member (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES("Philn", "Stewn", 3, 9);
 
-INSERT INTO team_member (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES("Philnp", "Stewnl", 3, 9);
 
-INSERT INTO team_member (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES("Philnp", "Stewnl", 3, 9);
 
-SELECT department.d_name, e_role.salary, e_role.title, team_member.last_name, team_member.first_name 
-FROM department
-INNER JOIN e_role ON e_role.department_id = department.d_name
-INNER JOIN team_member ON team_member.role_id = e_role.id
+SELECT team_member.last_name, team_member.first_name role.title AS role.id, role.salary AS salary, CONCAT(manager.first_name, ' ',manager.last_name) AS manager, department.department AS department
+FROM employee
+LEFT JOIN role ON role.id = role.title
+LEFT JOIN team_member ON team_member.role_id = e_role.id
 ORDER BY team_member.first_name;
 
+
+SELECT empolyee.first_name, Employee.last_name, role.salary
+FROM ((department INNER JOIN Employee ON department.department = employee.department_id)
+INNER JOIN role ON role.title = employee.role_id);
---
+++ server.js
@@ -0,0 +1,352 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: '361010theone',
+  database: 'employee_db',
+});
+
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('hi good to goo')
+  runSearch()
+});
+
+const runSearch = () => {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'View Employees',
+        'View Department',
+        'View Roles',
+        'Add Employee',
+        'Add Role',
+        'Update Employee Role',
+        'Update Manager',
+        'View Roles',
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'View  Employees':
+          employeeSearch();
+          break;
+
+        case 'View Department':
+          departmentSearch();
+          break;
+
+        case 'View By Manager':
+          viewManager();
+          break;
+        case 'View Roles':
+          roleSearch();
+          break;
+        case 'Add Employee':
+          addEmployee();
+          break;
+
+        case 'Add A Role':
+          addRole();
+          break;
+
+        case 'Add Department':
+          AddDepartment();
+          break;
+        case 'Update Employee Role':
+          updateRole();
+          break;
+
+
+
+
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+};
+
+
+let rolesArr = [];
+function getRoles() {
+  return connection.query("SELECT * FROM role", function (error, result) {
+    if (error) {
+      console.log(error);
+    }
+    result.forEach((role) => {
+      rolesArr.push({
+        name: role.title,
+        value: role.id,
+      });
+    });
+  });
+}
+// it's invoked at the start, doesn't need to be invoked again.
+getRoles();
+
+let employeeArr = [];
+function getEmployee() {
+  return connection.query("SELECT * FROM employee", function (error, result) {
+    if (error) {
+      console.log(error);
+    }
+    result.forEach((employee) => {
+      employeeArr.push({
+        name: employee.first_name,
+        name: employee.last_name,
+        value: employee.id
+      });
+    });
+  });
+}
+// it's invoked at the start, doesn't need to be invoked again.
+getEmployee();
+
+let departmentArr = [];
+function getDepartment() {
+  return connection.query("SELECT * FROM department", function (error, result) {
+    if (error) {
+      console.log(error);
+    }
+    result.forEach((department) => {
+      departmentArr.push({
+        name: department.department,
+        value: department.id
+      });
+    });
+  });
+}
+// it's invoked at the start, doesn't need to be invoked again.
+getDepartment();
+
+
+let managerArr = [];
+function getManager() {
+  return connection.query("SELECT first_name, last_name, role_id FROM employee WHERE role_id = 3", function (error, result) {
+    if (error) {
+      console.log(error);
+    }
+    result.forEach((employee) => {
+      managerArr.push({
+        name: employee.first_name,
+        name1: employee.last_name,
+        value: employee.role_id
+      });
+    });
+  });
+}
+// it's invoked at the start, doesn't need to be invoked again.
+getManager();
+
+const employeeSearch = () => {
+
+  const query = connection.query('SELECT * FROM employee', (err, res) => {
+    if (err) throw err;
+    res.forEach(({ id, first_name, last_name, role_id, manager_id }) => {
+      console.log(`${id} | ${first_name} | ${last_name} | ${role_id} | ${manager_id}`)
+    });
+    console.log('-------------------------')
+    runSearch()
+  });
+
+};
+
+const departmentSearch = () => {
+
+  const query = connection.query('SELECT * FROM department', (err, res) => {
+    if (err) throw err;
+    res.forEach(({ department }) => {
+      console.log(`${department}`)
+    });
+    console.log('-------------------------')
+
+  });
+  runSearch()
+};
+
+const roleSearch = () => {
+
+  connection.query('SELECT * FROM role', (err, res) => {
+    if (err) throw err;
+    res.forEach(({ id, title, salary }) => {
+      console.log(`${id} ${title} | ${salary} `)
+    });
+    console.log('-------------------------')
+  });
+  runSearch()
+};
+
+
+
+const viewManager = () => {
+
+  connection.query('SELECT * FROM employee WHERE role_id = 3', (err, res) => {
+    if (err) throw err;
+    res.forEach(({ first_name, last_name }) => {
+      console.log(`${first_name} | ${last_name} `)
+    });
+    console.log('-------------------------')
+  });
+  runSearch()
+};
+
+const addEmployee = () => {
+
+
+  inquirer
+    .prompt([{
+      name: 'first_name',
+      type: 'input',
+      message: 'What is the first name of the employee?',
+    },
+
+    {
+      name: 'last_name',
+      type: 'input',
+      message: 'What is the last name of the employee?',
+    },
+    {
+      name: 'Department',
+      type: 'rawlist',
+      message: 'What department were they hired for?',
+      choices: departmentArr,
+    },
+    {
+      name: 'role',
+      type: 'rawlist',
+      message: 'What position were they hired for?',
+      choices: rolesArr
+    },
+    {
+      name: 'manager_id',
+      type: 'rawlist',
+      message: "Who is this employee's manager?",
+      choices: managerArr
+
+    }
+
+    ])
+    .then((res) => {
+      console.log('Inserting a new product...\n');
+      console.log(employeeArr)
+      connection.query('INSERT INTO employee SET ?',
+
+        {
+          first_name: res.first_name,
+          last_name: res.last_name,
+          role_id: res.role_id,
+          manager_id: res.manager_id,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`${res.affectedRows} product inserted!\n`);
+          // Call updateProduct AFTER the INSERT completes
+          employeeSearch();
+        }
+      );
+
+
+    })
+
+}
+
+
+const addRole = () => {
+
+
+  inquirer
+    .prompt([{
+      name: 'deparment',
+      type: 'input',
+      message: 'What is the name of the nerw department?',
+    },
+  ])
+  .then((res) => {
+    console.log('Inserting a new department...\n');
+    console.log(employeeArr)
+    connection.query('INSERT INTO department SET ?',
+
+      {
+        deepartment: res.deepartment
+      },
+      (err, res) => {
+        if (err) throw err;
+        console.log(`${res.affectedRows} product inserted!\n`);
+        // Call updateProduct AFTER the INSERT completes
+        employeeSearch();
+      }
+    );
+
+
+  })
+
+
+
+
+}
+
+
+
+const updateRole = () => {
+  console.log(employeeArr)
+  inquirer
+    .prompt([
+      {
+        name: 'employee',
+        type: 'rawlist',
+        message: 'What employee do you need to update?',
+        choices: employeeArr
+      },
+      {
+        name: 'role',
+        type: 'rawlist',
+        message: 'What position to update too..',
+        choices: rolesArr
+      },
+
+    ]).then((res) => {
+      console.log(res.employee)
+      console.log(res.role)
+      console.log('Inserting a new product...\n');
+      connection.query('UPDATE employee SET employee.role_id = ? WHERE employee.id = ?',
+
+        [
+          res.role,
+
+
+          res.employee
+
+        ],
+        (err, res) => {
+          if (err) throw err;
+          console.log(`${res.affectedRows} product inserted!\n`);
+          // Call updateProduct AFTER the INSERT completes
+          getEmployee()
+          runSearch()
+
+        }
+      );
+
+
+
+    })
+
+  // logs the actual query being run
+
+};
+
